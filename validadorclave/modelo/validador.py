6a3b1c80e6db517d1f8951b960fe52ed0d0fac44
belac <belacaste3@gmail.com>
2023-05-19 22:26:18 -0500

    ejercicio

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
+from validadorclave.modelo import errores
+from errores import *
+
 
 class ReglaValidacion(ABC):
     _longitud_esperada: int
 
-    def _init_(self, longitud_esperada: int):
+    def __init__(self, longitud_esperada: int):
         self.longitud_esperada: int = longitud_esperada
 
     def _validar_longitud(self, clave: str) -> bool:
@@ -17,45 +20,48 @@ class ReglaValidacion(ABC):
             if letra.isupper():
                 return True
             else:
-                return False
+                raise NoTieneLetraMayusculaError("La clave debe contener letras mayusculas")
 
-    def contiene_minuscula(self, clave: str) -> bool:
+    def _contiene_minuscula(self, clave: str) -> bool:
         for letra in clave:
-            if letra.isupper():
-                return False
+            if letra.islower():
+                 return True
             else:
-                return True
+                raise NoTieneLetraMinusculaError("La clave debe contener letras minusculas")
 
-    def contiene_numero(self, clave: str) -> bool:
+    def _contiene_numero(self, clave: str) -> bool:
         for letra in clave:
             if letra.isdigit():
                 return True
             else:
-                return False
+                raise NoTieneNumeroError
 
     @abstractmethod
     def es_valida(self, clave) -> bool:
-        pass
+        raise
 
 
 class ReglaValidacionGanimedes(ReglaValidacion):
-    def _init_(self):
-        super()._init_(longitud_esperada=8)
+    def __init__(self):
+        super().__init__(longitud_esperada=8)
         self.longitud_esperada = 8
+        self.reglaValidacion = ReglaValidacion
 
-    @staticmethod
-    def contiene_caracter_especial(clave: str) -> bool:
-        if not re.match("^[a-zA-Z]+$", clave):
-            return True
-        else:
-            return False
+    def contiene_caracter_especial(self, clave):
+        caracteres_especiales = ['@', '_', '#', '$', '%']
+        if not any(c in caracteres_especiales for c in clave):
+            raise NoTieneCaracterEspecialError("La clave debe contener al menos uno de los siguientes caracteres especiales: @ _ # $ %")
+
     def es_valida(self, clave) -> bool:
-        pass
-
+        self._validar_longitud(clave)
+        self._contiene_mayuscula(clave)
+        self._contiene_minuscula(clave)
+        self._contiene_numero(clave)
+        self.contiene_caracter_especial(clave)
 
 class ReglaValidacionCalisto(ReglaValidacion):
-    def _init_(self):
-        super()._init_(longitud_esperada=6)
+    def __init__(self):
+        super().__init__(longitud_esperada=6)
 
     def contiene_calisto(self, clave):
         contador_mayusculas = 0
@@ -68,5 +74,19 @@ class ReglaValidacionCalisto(ReglaValidacion):
                     return True
             return False
 
-b = ReglaValidacionCalisto()
-print(b.es_valida("CAlisto123"))
+    def es_valida(self, clave) -> bool:
+        self._validar_longitud(clave)
+        self._contiene_mayuscula(clave)
+        self._contiene_minuscula(clave)
+        self._contiene_numero(clave)
+        self.contiene_calisto(clave)
+
+
+class Validador:
+    def __init__(self, regla: ReglaValidacion):
+        self.regla = regla
+
+    def es_valida(self, clave):
+        return self.regla.es_valida(clave)
+
+
